fecc16badd9483c115f135eb7a62df6e09ceceb2
okamuuu <okamuuu@gmail.com>
2010-12-26 14:47:28 +0900

    test

--- setup.sh
+++ setup.sh
@@ -1,6 +1,13 @@
 cd $HOME/project/dotfiles
-ln -sf `pwd`/vim/vimrc                         $HOME/.vimrc
-ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
-ln -sf `pwd`/zsh/zshrc                         $HOME/.zshrc
-ln -sf `pwd`/screen/screenrc                   $HOME/.screenrc
+
+ln -sf `pwd`/vim/vimrc $HOME/.vimrc
+
+mkdir -p $HOME/.vim/autoload
+ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/pathogen.vim $HOME/.vim/autoload/pathogen.vim
+
+touch $HOME/.zshrc_custom
+ln -sf `pwd`/zsh/zshrc $HOME/.zshrc
+ln -sf `pwd`/zsh/oh-my-zsh $HOME/.oh-my-zsh
+
+ln -sf `pwd`/screen/screenrc $HOME/.screenrc
 
