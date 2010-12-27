0c7fc9a35ed95a8273dffee25b1af0d898c72266
okamuuu <okamuuu@gmail.com>
2010-12-27 11:03:09 +0900

    zshrc -> zshenv

--- setup.sh
+++ setup.sh
@@ -7,6 +7,7 @@ ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
 
 touch $HOME/.zshrc_custom
 ln -sf `pwd`/zsh/zshrc $HOME/.zshrc
+ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
 ln -sf `pwd`/zsh/oh-my-zsh $HOME/.oh-my-zsh
 
 ln -sf `pwd`/screen/screenrc $HOME/.screenrc
