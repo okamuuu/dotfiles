1651251a376ee8837af73558735344fcc5b2b388
okamuuu <okamuuu@gmail.com>
2011-01-10 12:09:21 +0900

    ci

--- setup.sh
+++ setup.sh
@@ -6,7 +6,6 @@ ln -sf `pwd`/vim/bundle/ $HOME/.vim/bundle
 ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
 
 touch $HOME/.zshrc_custom
-ln -sf `pwd`/zsh/zshrc $HOME/.zshrc
 ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
 ln -sf `pwd`/zsh/oh-my-zsh $HOME/.oh-my-zsh
 
