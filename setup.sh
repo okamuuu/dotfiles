0188c9858347033805e76f996955f549d2398e7c
okamuuu <okamuuu@gmail.com>
2011-05-27 22:45:08 +0900

    vundleに変更してるのよー

--- setup.sh
+++ setup.sh
@@ -1,14 +1,17 @@
 cd $HOME/project/dotfiles
 
-mkdir $HOME/.ssh
-mkdir $HOME/.vim
+mkdir -p $HOME/.ssh
+mkdir -p $HOME/.vim
 ln -sf `pwd`/vim/vimrc $HOME/.vimrc
 
-ln -sf `pwd`/vim/bundle/ $HOME/.vim/bundle
-ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
+git submodule init
+git submodule update    
+#ln -sf `pwd`/vim/bundle/ $HOME/.vim/bundle
+#ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
 
 touch $HOME/.zshrc_custom
 ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
 
 ln -sf `pwd`/screen/screenrc $HOME/.screenrc
+
 
