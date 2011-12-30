22a7ef7bf491b741c5d9b75e4295e3fe7be0f7a0
okamuuu <okamuuu@gmail.com>
2011-12-30 11:56:41 +0900

    added jslint

--- setup.sh
+++ setup.sh
@@ -1,17 +1,17 @@
 cd $HOME/project/dotfiles
 
 mkdir -p $HOME/.ssh
-mkdir -p $HOME/.vim
+mkdir -p $HOME/.vim/tmp
 ln -sf `pwd`/vim/vimrc $HOME/.vimrc
+ln -sf `pwd`/vim/compiler $HOME/.vim/compiler
 
 git submodule init
 git submodule update    
-#ln -sf `pwd`/vim/bundle/ $HOME/.vim/bundle
-#ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
 
 touch $HOME/.zshrc_custom
 ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
 
 ln -sf `pwd`/screen/screenrc $HOME/.screenrc
+ln -sf `pwd`/jslint/jslint $HOME/.jslintrc
 
 
