fa0e337459cc09225e132d2c567da01356c57b12
okamuuu <okamuuu@gmail.com>
2012-01-30 14:25:37 +0900

    fixed neobundle path.

--- setup.sh
+++ setup.sh
@@ -1,20 +1,19 @@
 cd $HOME/project/dotfiles
 
+git submodule init
+git submodule update    
+
 mkdir -p $HOME/.ssh
 mkdir -p $HOME/.vim/tmp
 ln -sf `pwd`/vim/vimrc $HOME/.vimrc
 ln -sf `pwd`/vim/compiler $HOME/.vim/compiler
-
-git submodule init
-git submodule update    
-
-ln -sf `pwd`/vim/bundle/ $HOME/.vim/bundle
-#ln -sf `pwd`/vim/bundle/vim-pathogen/autoload/ $HOME/.vim/autoload
+ln -sf `pwd`/vim/neobundle.vim/ $HOME/.vim/neobundle.vim
 
 touch $HOME/.zshrc_custom
 ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
 
 ln -sf `pwd`/screen/screenrc $HOME/.screenrc
-ln -sf `pwd`/jslint/jslint $HOME/.jslintrc
+
+#ln -sf `pwd`/jslint/jslint $HOME/.jslintrc
 
 
