0eb89b71137203905c8198bf892f8be8309c430b
okamura masao <okamuuu@okamura-no-MacBook-Pro.local>
2013-03-02 16:41:09 +0900

    #

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ git submodule update
 
 mkdir -p $HOME/.ssh
 mkdir -p $HOME/.vim/tmp
+mkdir -p $HOME/.vim/bundle
 ln -sf `pwd`/vim/vimrc $HOME/.vimrc
-ln -sf `pwd`/vim/neobundle.vim $HOME/.vim/neobundle.vim
+ln -sf `pwd`/vim/neobundle.vim $HOME/.vim/bundle/neobundle.vim
 
 touch $HOME/.zshrc_custom
 ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
