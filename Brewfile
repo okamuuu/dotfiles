80c705c831c035de8d845722556c816efa707e49
okamuuu <okamuuu@gmail.com>
2014-05-21 09:52:20 +0900

    added Brewfile

---
+++ Brewfile
@@ -0,0 +1,20 @@
+update | true
+upgrade | true
+
+install git 
+install zsh 
+install screen
+
+tap phinze/homebrew-cask
+install brew-cask
+
+cask install mou 
+cask install coteditor
+cask install google-chrome 
+cask install firefox
+cask install dropbox
+cask install virtualbox 
+cask install vagrant
+cask install genymotion
+cask install hipchat skype
+cask install vlc
--- setup.sh
+++ setup.sh
@@ -1,22 +1,19 @@
-git submodule init
-git submodule update    
-git submodule foreach 'git pull origin master'
+## brew
+ruby -e '"$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/installe\)"'
+brew tap phinze/homebrew-cask
+brew install brew-cask
+brew bundle
 
-mkdir -p $HOME/.ssh
-mkdir -p $HOME/.vim/tmp
-#rm -fr $HOME/.vim/bundle
+## vim
 mkdir -p $HOME/.vim/bundle
 ln -sf `pwd`/vim/vimrc $HOME/.vimrc
 ln -sf `pwd`/vim/neobundle.vim $HOME/.vim/bundle/neobundle.vim
 
+## zsh
 touch $HOME/.zshrc
 ln -sf `pwd`/zsh/zshenv $HOME/.zshenv
 
+## screen
 ln -sf `pwd`/screen/screenrc $HOME/.screenrc
-#ln -sf `pwd`/jshint/jshintrc $HOME/.jshintrc
 
-#ln -sf `pwd`/jslint/jslint $HOME/.jslintrc
-#~/.vim/bundle/vimproc/autoload
-#make -f make_mac.mak
-#make -f make_gcc.mak
 
