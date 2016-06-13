64e09f05be8f0175fbee117e7f01a7300e972905
okamuuu <okamuuu@gmail.com>
2016-06-13 17:00:36 +0900

    replace NeoBundle to Dein

--- setup.sh
+++ setup.sh
@@ -1,14 +1,6 @@
-## submodules
-git submodule init
-git submodule foreach 'git pull origin master'
-git submodule update
-
 ## vim
 mkdir -p $HOME/.vim/bundle
 ln -sf `pwd`/vim/vimrc $HOME/.vimrc
-if [ ! -e $HOME/.vim/bundle/neobundle.vim ]; then
-    ln -sf `pwd`/submodules/neobundle.vim $HOME/.vim/bundle/neobundle.vim
-fi
 
 ## zsh
 touch $HOME/.zshrc
